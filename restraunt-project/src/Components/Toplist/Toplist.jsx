d841526b14bb7343f1986bf3610e2538b8f56098
AnjaliShukla12345 <as7277563@gmail.com>
2024-12-25 16:21:40 +0530

    first commit

--- restraunt-project/src/Components/Toplist/Toplist.jsx
+++ restraunt-project/src/Components/Toplist/Toplist.jsx
@@ -11,24 +11,27 @@ const FoodData = [
     {
         image: rasgulla,
         rating:"⭐⭐⭐⭐",
-        price: "$10.99",
+       price: "$12.55",
         name: "Rasgulla",
         desc: "Rasgulla is made by curdling milk to form chenna, kneading it into smooth balls, and boiling them in sugar syrup until soft and spongy. Serve chilled for a delightful treat!"
     },
+
     {
         image: samosa,
         rating:"⭐⭐⭐⭐",
-        price: "$10.99",
+      price: "$16.66",
         name: "Samosa",
         desc: "Samosa is made by filling spiced mashed potatoes in a dough pocket, folding into a triangle, and deep-frying until crispy. Serve with chutney for a savory snack!"
     },
+
     {
         image: chhole,
         rating:"⭐⭐⭐",
-        price: "$10.99",
+        price: "$8.99",
         name: "Chhole",
         desc: "Chhola is made by soaking chickpeas, cooking them with onions, tomatoes, and aromatic spices like cumin, turmeric, and garam masala. Garnish with cilantro and serve with rice or bread."
     },
+
     {
         image: chhoow,
         rating:"⭐⭐",
@@ -36,21 +39,23 @@ const FoodData = [
         name: "Chowmin",
      desc: "Chow mein is made by stir-frying boiled noodles with vegetables, soy sauce, garlic, ginger, and optional protein like chicken or tofu, for a savory, aromatic dish."
     },
+
     {
         image: plate,
         rating:"⭐⭐⭐⭐",
-        price: "$10.99",
+        price: "$11.22",
         name: "Veg Paneer",
          desc: "Sauté onions, tomatoes, and spices to make a rich gravy. Add diced paneer and vegetables like capsicum or peas. Simmer until cooked. Garnish with coriander. Serve hot!"
     },
+
     {
         image: salad,
-        rating:"⭐⭐⭐⭐",
-        price: "$10.99",
+        rating:"⭐⭐⭐",
+        price: "$9.99",
         name: "Veg Salad",
          desc: "For a veggie salad, combine chopped cucumbers, tomatoes, carrots, onions, and bell peppers. Toss with olive oil, lemon juice, salt, and pepper for a refreshing dish!"
     }
-]
+];
 
 
 
@@ -82,7 +87,11 @@ const Toplist = () => {
     <div className='space-y-2'>
 
     <p className='text-red-500'>{item.rating}</p>
+
+    
+
     <p className='text-lg font-semibold'>{item.name}</p>
+   
 
     <p>{item.desc}</p>
 
